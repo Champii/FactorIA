56cbaef07b131cc6d8357f43742d6e1db77a1ccc
Berelindis <romain.bellande@orange.fr>
2016-08-31 23:43:10 +0200

    add jsdoc

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,34 @@
 var gulp        = require('gulp'),
 livescript      = require('gulp-livescript'),
-nodemon         = require('gulp-nodemon');
+nodemon         = require('gulp-nodemon'),
+server          = require('gulp-server-livereload'),
+jsdoc           = require('gulp-jsdoc3'),
+runSequence     = require('run-sequence');
 
 var paths = {
   ls: './src/**/*.ls',
-  build: './build'
+  build: './build',
+  js: ['!./build/node_modules', './build/**/*.js'],
+  jsdoc: './docs/gen'
 };
+
+gulp.task('doc', ['ls'], function () {
+  return gulp.src(['README.md'].concat(paths.js), {read: false})
+    .pipe(jsdoc());
+});
+
+gulp.task('open-doc', function () {
+ gulp.src(paths.jsdoc)
+  .pipe(server({
+    livereload: {
+      enable: true,
+      port: 35730
+    },
+    directoryListing: false,
+    open: true,
+    port: 8001
+  }));
+});
 
 gulp.task('ls', function() {
   return gulp.src(paths.ls)
@@ -14,7 +37,9 @@ gulp.task('ls', function() {
 });
 
 gulp.task('watch', function () {
-  gulp.watch(paths.ls, ['ls']);
+  gulp.watch(paths.ls, ['doc']);
 });
 
-gulp.task('default', ['watch']);
+gulp.task('default', function () {
+  runSequence('watch', 'open-doc');
+});
--- src/index.ls
+++ src/index.ls
@@ -8,15 +8,18 @@ require! {
 class FactorIA
 
   ->
+
     @app = express!
     @server = http.Server @app
 
     @io = socket @server
 
+
     Player
       .create login: \tontonTest2
       .then -> it.login = 'whesh'
       .then -> Player.fetch login: \tontonTest2
+
       .then console.log
 
 module.exports = FactorIA
